b3ce1ce7f5d9673b8db10e9125b9747803c642bf
ShivomaA <shivoma@makemywebsite.com.au>
2026-04-10 15:05:36 +1000

    form settings update

--- forms.js
+++ forms.js
@@ -8,15 +8,15 @@
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         body: body
       })
-        // .then(function () {
-        //   var msg = document.createElement('p');
-        //   msg.textContent = "Thanks! We'll be in touch soon.";
-        //   msg.style.cssText = 'margin-top:1rem;font-weight:600;color:inherit;';
-        //   form.replaceWith(msg);
-        // })
         .then(function () {
-          window.location.href = '/thank-you/';
+          var msg = document.createElement('p');
+          msg.textContent = "Thanks! We'll be in touch soon.";
+          msg.style.cssText = 'margin-top:1rem;font-weight:600;color:inherit;';
+          form.replaceWith(msg);
         })
+        // .then(function () {
+        //   window.location.href = '/thank-you/';
+        // })
         .catch(function () {
           var msg = document.createElement('p');
           msg.textContent = 'Something went wrong. Please try again or call us on 1300 978 051.';
